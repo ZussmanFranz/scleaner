e3a953677936e47a9ec934c09f548baf8732fef2
Zussman <zhenya.pyryev@gmail.com>
2026-03-02 19:40:08 +0100

    fix debug filtering which was blocking custom debug messages

--- scleaner-project/main.cpp
+++ scleaner-project/main.cpp
@@ -1,9 +1,13 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QLoggingCategory>
 
 int main(int argc, char *argv[])
 {
+    // This forces Qt to unmute all debug messages globally
+    QLoggingCategory::setFilterRules("*.debug=false\ndefault.debug=true");
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
